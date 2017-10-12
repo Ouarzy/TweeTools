141589a1f23fac1994dcd8e7b50e34e2d0af8a20
ouarzy <epecoul@gmail.com>
2017-10-12 13:21:46 +0200

    Init commit

---
+++ TweeTools/Program.fs
@@ -0,0 +1,19 @@
+﻿module main
+
+open TweeterLogin
+open System
+
+
+let displayMentions2 mention =
+    printfn "%s" mention
+
+[<EntryPoint>]
+let main argv = 
+    printfn "starting log"
+    openPage |> ignore
+    printfn "Quelle est votre code d'autorisation?"
+    let verifierCode = Console.ReadLine()
+    let accessToken = getAccessToken verifierCode
+    displayMentions accessToken |> Seq.cast |> Seq.map displayMentions2 |> ignore
+    0 // return an integer exit code
+                           
---
+++ TweeTools/TweeterLogin.fs
@@ -0,0 +1,25 @@
+﻿module TweeterLogin
+
+open TweetSharp;
+
+// Pass your credentials to the service
+let twitterService = new TwitterService("FnqMRIndSaO1KDL8F1r8eZ6He", "FmqoNdZvwF3Q0RKsLI4XjGw5MFjYqKk05iId7PMxRR325KQBuT");
+
+// Step 1 - Retrieve an OAuth Request Token
+let requestToken = twitterService.GetRequestToken()
+
+// Step 2 - Redirect to the OAuth Authorization URL
+let uri = twitterService.GetAuthorizationUri(requestToken);
+let openPage = 
+    System.Diagnostics.Process.Start("chrome.exe", uri.ToString());
+
+// Step 3 - Exchange the Request Token for an Access Token
+let getAccessToken (verifier : string) = 
+    twitterService.GetAccessToken(requestToken, verifier)
+
+// Step 4 - User authenticates using the Access Token
+let displayMentions (accessToken : OAuthAccessToken) =
+    twitterService.AuthenticateWith(accessToken.Token, accessToken.TokenSecret)
+    let result = twitterService.ListTweetsMentioningMe(new ListTweetsMentioningMeOptions())
+    result
+
