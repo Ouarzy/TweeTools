2abc12d915d62fd8ded43a4a409444ebf910abe1
ouarzy <epecoul@gmail.com>
2017-10-12 18:32:18 +0200

    Advanced code

--- TweeTools/Program.fs
+++ TweeTools/Program.fs
@@ -10,20 +10,40 @@ let displayMentions2 (mention : TweetSharp.TwitterStatus) =
 let displayFollowers (follower : int64) =
     printfn "%i" follower
 
+let test (x : TwitterUser) = 
+    x
+
 [<EntryPoint>]
 let main argv = 
-    printfn "starting log"
+    printfn "Bonjour!"
     openPage |> ignore
     printfn "Quelle est votre code d'autorisation?"
     let verifierCode = Console.ReadLine()
     let accessToken = getAccessToken verifierCode
+    authenticateWith accessToken
+
+    printfn "Quelle compte voulez vous analyser?"
+    let expectedAccount = Console.ReadLine()
     
-    let mentions = getMentions accessToken
-    mentions |> Seq.cast |> Seq.iter displayMentions2
+    let followersIds = listFollowersOf expectedAccount
+    printfn "Le compte %s a %i followers" expectedAccount followersIds.Count
+    
+    printfn "Quelle mot clé souhaitez vous analyser dans la description des followers?"
+    let expectedkeyWord = Console.ReadLine()
+    
+    let allDescriptions = followersIds |> Seq.map getDescriptionUser
+    let allUserIdsWithMatchingKeyword = allDescriptions |> Seq.filter (fun twitterUser -> twitterUser.Description.Contains(expectedkeyWord)) |> Seq.toArray
 
-    let followers = listFollowersOf accessToken
-    followers |> Seq.cast |> Seq.iter displayFollowers
+    printfn "Il y a %i potentiels personnes à follow, voulez vous le faire? (Y/N)" allUserIdsWithMatchingKeyword.Length
+    let answer = Console.ReadLine()
 
+    let t = followUser {Id = 12; Description = "toto"}
+
+    if answer.ToLower().Equals("Y") then
+        allUserIdsWithMatchingKeyword |> Array.map test
+        //(fun x -> followUser x)
+
+    printfn "Fin..."
     Console.ReadLine() |> ignore
     0 // return an integer exit code
                            
--- TweeTools/TweeterLogin.fs
+++ TweeTools/TweeterLogin.fs
@@ -1,49 +1,64 @@
 ﻿module TweeterLogin
 
 open TweetSharp;
+open System
 
-// Pass your credentials to the service
+type TwitterUser = { Id : int64 ; Description : string}
+
 let twitterService = new TwitterService("FnqMRIndSaO1KDL8F1r8eZ6He", "FmqoNdZvwF3Q0RKsLI4XjGw5MFjYqKk05iId7PMxRR325KQBuT");
 
-// Step 1 - Retrieve an OAuth Request Token
 let requestToken = twitterService.GetRequestToken()
 
-// Step 2 - Redirect to the OAuth Authorization URL
 let uri = twitterService.GetAuthorizationUri(requestToken);
-let openPage = 
-    System.Diagnostics.Process.Start("chrome.exe", uri.ToString());
+let openPage =  System.Diagnostics.Process.Start("chrome.exe", uri.ToString());
 
-// Step 3 - Exchange the Request Token for an Access Token
 let getAccessToken (verifier : string) = 
     twitterService.GetAccessToken(requestToken, verifier)
 
 let listTweetsMentioningMeOptions ()= 
        let option = new ListTweetsMentioningMeOptions()
-       option.ContributorDetails <- new System.Nullable<bool>(true)
+       option.ContributorDetails <- new Nullable<bool>(true)
        option
 
-// Step 4 - User authenticates using the Access Token
-let getMentions (accessToken : OAuthAccessToken) =
+let authenticateWith(accessToken : OAuthAccessToken) =
     twitterService.AuthenticateWith(accessToken.Token, accessToken.TokenSecret)
+
+let getMentions () =
     let result = twitterService.ListTweetsMentioningMe(listTweetsMentioningMeOptions())
     result
 
-let userProfileForOption ()=
+let userProfileForOption userName=
         let option =new GetUserProfileForOptions()
-        option.ScreenName <- "lacuisineduweb"
+        option.ScreenName <- userName
         option
 
-let getuserProfileFor (accessToken : OAuthAccessToken) =
-    twitterService.AuthenticateWith(accessToken.Token, accessToken.TokenSecret)
-    let result = twitterService.GetUserProfileFor(userProfileForOption())
+let getuserProfileFor userName =
+    let result = twitterService.GetUserProfileFor(userProfileForOption(userName))
     result
 
-let listFollowerIdsOfOptions () =
+let listFollowerIdsOfOptions userName =
         let option =new ListFollowerIdsOfOptions()
-        option.ScreenName <- "lacuisineduweb"
+        option.ScreenName <- userName
         option
 
-let listFollowersOf (accessToken : OAuthAccessToken) =
-    twitterService.AuthenticateWith(accessToken.Token, accessToken.TokenSecret)
-    let result = twitterService.ListFollowerIdsOf(listFollowerIdsOfOptions())
+let listFollowersOf userName =
+    let result = twitterService.ListFollowerIdsOf(listFollowerIdsOfOptions(userName))
+    result
+
+let getUserProfileForOptions (userId : int64) =
+    let option =new GetUserProfileForOptions()
+    option.UserId <- new Nullable<int64>(userId)
+    option
+
+let getDescriptionUser (userId : int64) =
+    let result = twitterService.GetUserProfileFor(getUserProfileForOptions(userId))
+    {Id = result.Id; Description = result.Description}
+
+let followUserOptions (userId : int64) =
+    let option =new FollowUserOptions()
+    option.UserId <- new Nullable<int64>(userId)
+    option
+
+let followUser (user : TwitterUser) =
+    let result = twitterService.FollowUser(followUserOptions(user.Id))
     result
